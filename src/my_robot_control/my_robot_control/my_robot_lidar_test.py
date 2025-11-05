037461e97ef4dc30f4258fa2d22da533b535780d
Dr. Manel Puig i Vidal <36498743+manelpuig@users.noreply.github.com>
2025-11-05 16:35:19 +0100

    updated project activities

--- src/my_robot_control/my_robot_control/my_robot_lidar_test.py
+++ src/my_robot_control/my_robot_control/my_robot_lidar_test.py
@@ -14,10 +14,27 @@ class LidarTest(Node):
             self.listener_callback,
             10
         )
+        self.scan_msg_shown = False
+        self.last_print_time = self.get_clock().now().seconds_nanoseconds()[0]
 
     def listener_callback(self, scan):
+        current_time = self.get_clock().now().seconds_nanoseconds()[0]
+        if current_time - self.last_print_time < 1:
+            return  # Skip printing if less than 1 second has passed
+
         angle_min_deg = scan.angle_min * 180.0 / 3.14159
+        angle_max_deg = scan.angle_max * 180.0 / 3.14159
+        range_deg = angle_max_deg - angle_min_deg
         angle_increment_deg = scan.angle_increment * 180.0 / 3.14159
+        total_points = len(scan.ranges)
+
+        if not self.scan_msg_shown:
+            self.get_logger().info(f"Min angle: {angle_min_deg:.2f} deg")
+            self.get_logger().info(f"Max angle: {angle_max_deg:.2f} deg")
+            self.get_logger().info(f"Total lidar range angle: {range_deg:.2f} deg")
+            self.get_logger().info(f"Delta angle per index: {angle_increment_deg:.2f} deg")
+            self.get_logger().info(f"Total number of Laser Beams: {total_points}")
+            self.scan_msg_shown = True
 
         # Indices for specific angles
         index_0_deg = int((0.0 - angle_min_deg) / angle_increment_deg)
@@ -30,13 +47,13 @@ class LidarTest(Node):
             angle_deg = angle_min_deg + i * angle_increment_deg
             if distance == float('inf') or distance == 0.0:
                 continue
-            if -150 <= angle <= 150:
+            if -150 <= angle_deg <= 150:
                 custom_range.append((distance, i))
 
         if custom_range:
             closest_distance, element_index = min(custom_range)
             angle_closest_distance = angle_min_deg + element_index * angle_increment_deg
-            
+
             dist_0_deg = scan.ranges[index_0_deg]
             dist_neg90_deg = scan.ranges[index_neg90_deg]
             dist_pos90_deg = scan.ranges[index_pos90_deg]
@@ -48,6 +65,9 @@ class LidarTest(Node):
             self.get_logger().info(f"Minimum distance: {closest_distance:.2f} m at angle {angle_closest_distance:.2f}°")
         else:
             self.get_logger().info("No valid readings in range [-150°, 150°]")
+
+        self.last_print_time = current_time
+
 
 def main(args=None):
     rclpy.init(args=args)
